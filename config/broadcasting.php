64bb5893ce3407061a974f1ac2eabfd4f160be0a
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2022-06-05 19:14:40 +0200

    initial commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -43,7 +43,8 @@ return [
                 'scheme' => 'http'
             ],
             'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
             ],
         ],
 
