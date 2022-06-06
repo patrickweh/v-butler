23ae6f99cf0349108159e6bebcddba4865b95c5a
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2022-06-06 19:00:14 +0200

    add env keys

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="manifest" href="/manifest.json">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{config('app.name')}}</title>
