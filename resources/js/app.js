35d91fe1278e5f47a10750b061b1806c6caf7bdb
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2022-08-05 13:26:22 +0200

    add hot reload for livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import focus from '@alpinejs/focus'
 import * as Turbo from "@hotwired/turbo";
 import 'livewire-turbolinks';
 import './features/dark-mode'
+import {livewire_hot_reload} from 'virtual:livewire-hot-reload'
 
 Alpine.plugin(focus)
 
@@ -10,3 +11,5 @@ window.Alpine = Alpine;
 Alpine.start();
 
 export default Turbo;
+
+livewire_hot_reload();
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import livewire from '@defstudio/vite-livewire-plugin';
 
 export default defineConfig({
     plugins: [
@@ -8,5 +9,8 @@ export default defineConfig({
             'resources/js/app.js',
             'resources/js/features/echo.js',
         ]),
+        livewire({  // <-- add livewire plugin
+            refresh: ['resources/css/app.css'],  // <-- will refresh css (tailwind ) as well
+        }),
     ],
 });
