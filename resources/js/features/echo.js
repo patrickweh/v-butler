847fd9d00989569ee071dfd1f682a14e35787083
Patrick Weh <40495041+patrickweh@users.noreply.github.com>
2023-04-01 21:44:47 +0200

    fix echo

--- resources/js/features/echo.js
+++ resources/js/features/echo.js
@@ -1,15 +1,17 @@
 import Echo from 'laravel-echo'
 
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    wssPort: 6001,
-    enabledTransports: ['ws', 'wss'],
+    cluster:import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    wsHost: window.location.hostname, // <-- important if you dont build the js file on the prod server
     forceTLS: false,
+    wsPort: 80,
+    wssPort: 443, // <-- this ensures that nginx will receive the request
     disableStats: true,
+    enabledTransports: ['ws', 'wss'],
 });
